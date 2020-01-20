3630bb94c1a5e0bd7fb6e5e9ce3a41513e4825e6
zhpanvip <zhpanvip@outlook.com>
2020-01-20 19:30:37 +0800

    Add Indicator Sample

--- app/src/main/java/com/zhpan/viewpagerindicator/MainActivity.java
+++ app/src/main/java/com/zhpan/viewpagerindicator/MainActivity.java
@@ -1,8 +1,18 @@
 package com.zhpan.viewpagerindicator;
 
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.zhpan.indicatorview.IndicatorView;
+import com.zhpan.indicatorview.enums.IndicatorSlideMode;
+import com.zhpan.indicatorview.enums.IndicatorStyle;
+import com.zhpan.indicatorview.model.IndicatorOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.banner_view);
+        final IndicatorView indicatorView = findViewById(R.id.indicator_view);
+        indicatorView.setIndicatorOptions(getIndicatorOption());
+        indicatorView.setPageSize(getData().size());
+        viewPager.setAdapter(new ViewPagerAdapter(getData()));
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                indicatorView.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                indicatorView.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                indicatorView.onPageScrollStateChanged(state);
+            }
+        });
+    }
+
+    private IndicatorOptions getIndicatorOption() {
+        IndicatorOptions options = new IndicatorOptions();
+        options.setCheckedColor(getResColor(R.color.red_checked_color));
+        options.setNormalColor(getResColor(R.color.red_normal_color));
+        options.setSlideMode(IndicatorSlideMode.WORM);
+        options.setCheckedIndicatorWidth(getResources().getDimensionPixelOffset(R.dimen.dp_20));
+        options.setNormalIndicatorWidth(getResources().getDimensionPixelOffset(R.dimen.dp_20));
+        options.setIndicatorStyle(IndicatorStyle.ROUND_RECT);
+        return options;
+    }
+
+    protected List<Integer> getData() {
+        List<Integer> mDrawableList = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            int drawable = getResources().getIdentifier("color" + i, "color", getPackageName());
+            mDrawableList.add(drawable);
+        }
+        return mDrawableList;
+    }
+
+    private int getResColor(@ColorRes int color) {
+        return getResources().getColor(color);
     }
 }
---
+++ app/src/main/java/com/zhpan/viewpagerindicator/ViewPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.zhpan.viewpagerindicator;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <pre>
+ *   Created by zhangpan on 2020-01-20.
+ *   Description:
+ * </pre>
+ */
+public class ViewPagerAdapter extends PagerAdapter {
+    private List<Integer> mDrawableList = new ArrayList<>();
+
+    public ViewPagerAdapter(List<Integer> drawableList) {
+        mDrawableList.addAll(drawableList);
+    }
+
+    @Override
+    public int getCount() {
+        return mDrawableList.size();
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void finishUpdate(@NonNull ViewGroup container) {
+        super.finishUpdate(container);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.item_slide_mode, container, false);
+        ImageView imageView = view.findViewById(R.id.banner_image);
+        imageView.setBackgroundColor(container.getContext().getResources().getColor(mDrawableList.get(position)));
+        container.addView(view);
+        return view;
+    }
+}
